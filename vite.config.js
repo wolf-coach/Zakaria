70d3b86874179e7dbc3053a46a208467ed0286e0
wolf-coach <ramishirif@gmail.com>
2026-09-15 13:10:11 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs.plugin-react';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Zakaria/', // Replace 'repository-name' with your exact GitHub repo name
+  base: './',
 });
 
