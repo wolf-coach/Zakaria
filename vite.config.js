36e0ae893c89bfa70bc2f4d2f8f34c1a03a54e4b
wolf-coach <ramishirif@gmail.com>
2026-09-15 05:08:58 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs.plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/repository-name/', // Replace 'repository-name' with your exact GitHub repo name
+  base: '/Zakaria/', // Replace 'repository-name' with your exact GitHub repo name
 });
 
