e6f4a4e2fe619541f0432f608ac808184e8db310
wolf-coach <ramishirif@gmail.com>
2026-09-18 18:01:13 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -421,7 +421,7 @@ function Home({ go }) {
                     spaceBetween={30}
                     centeredSlides={true}
                     autoplay={{
-                      delay: 2500,
+                      delay: 2000,
                       disableOnInteraction: false,
                     }}
                     pagination={{
